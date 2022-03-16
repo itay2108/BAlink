59e808307bb864f63bbe4fef126229b91d128672
itay <gervash@icloud.com>
2022-03-16 10:17:32 +0200

    clean up code

--- BAlink/View/Controller/FavoritesViewController.swift
+++ BAlink/View/Controller/FavoritesViewController.swift
@@ -24,10 +24,7 @@ class FavoritesViewController: ProductsViewController {
     
     convenience init() {
         self.init(with: FavoritesViewModel())
-        //viewModel.delegate = self
     }
-    
-    
     
     override func setupUI() {
         super.setupUI()
@@ -40,13 +37,6 @@ class FavoritesViewController: ProductsViewController {
         }
     }
     
-//    override func viewDidAppear(_ animated: Bool) {
-//        
-//        viewModel.favoriteProducts = RealmManager.shared.realmFavorites(getProductsOf: viewModel.username)
-//        
-//        productGallery.reloadData()
-//    }
-    
     //override numberOfItems to also display empty label when data source empty
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -54,30 +44,6 @@ class FavoritesViewController: ProductsViewController {
         emptyDataLabel.isHidden = viewModel.dataSource().count == 0 ? false : true
         
         return super.collectionView(collectionView, numberOfItemsInSection: section)
-    }
-
-    //override product cell likeHandler to also reload data after modification (to remove unliked items from gallery)
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let originalCell = super.collectionView(collectionView, cellForItemAt: indexPath)
-
-        guard let cell = originalCell as? ProductCollectionViewCell,
-            viewModel.dataSource().count > indexPath.row else {
-            return originalCell
-        }
-
-        let product = viewModel.dataSource()[indexPath.row]
-
-        cell.likeHandler = { [weak self] in
-            if !cell.isLiked {
-                self?.viewModel.removeFromFavorites(product)
-                collectionView.reloadData()
-            } else {
-                self?.viewModel.addToFavorites(product)
-            }
-        }
-
-        return cell
     }
     
 }
